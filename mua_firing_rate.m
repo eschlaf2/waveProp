e3d20cab3c4b0a43c965609114405d247689533e
Emily <eds2@bu.edu>
2019-01-23 18:54:24 -0500

    Create mua_firing_rate.m

---
+++ mua_firing_rate.m
@@ -0,0 +1,9 @@
+function mea = mua_firing_rate(mea)
+% Returns the firing rate as computed in Smith et al., 2016. Units are in
+% spikes per second
+
+samplingRateMs = mea.SamplingRate / 1e3;  % samples per ms
+windMs = 100;
+window = windMs * samplingRateMs;  % number of samples to use in the window
+mea.firingRate = ...
+	smoothdata(mea.events, 1, 'movmean', window) * mea.SamplingRate;
