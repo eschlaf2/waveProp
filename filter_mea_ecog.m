0ab5e8c0ffc83524bb253efe7f4dd3bbfed8748c
Emily <eds2@bu.edu>
2019-02-06 11:52:14 -0500

    Create filter_mea_ecog.m

---
+++ filter_mea_ecog.m
@@ -0,0 +1,55 @@
+function [mea, ecog] = filter_mea_ecog(mea, ecog)
+
+
+MEA = exist('mea', 'var');
+ECOG = exist('ecog', 'var');
+
+if MEA
+	bpFilt = designfilt('bandpassfir','FilterOrder',150, ...
+		'CutoffFrequency1',2,'CutoffFrequency2',50, ...
+		'SampleRate', mea.SamplingRate);
+
+	mea.lfp = single(filtfilt(bpFilt, double(mea.Data)));
+	mea.lfp(:, mea.BadChannels) = [];
+	
+	bpFilt = designfilt('bandpassfir','FilterOrder',150, ...
+		'CutoffFrequency1',3e2,'CutoffFrequency2',3e3, ...
+		'SampleRate',mea.SamplingRate);
+	mea.mua = single(filtfilt(bpFilt, double(mea.Data)));
+	mea.mua(:, mea.BadChannels) = [];
+
+	bpFilt = designfilt('bandpassfir', 'FilterOrder', 150, ...
+		'CutoffFrequency1', 50, 'CutoffFrequency2', 300, ...
+		'SampleRate', mea.SamplingRate);
+	mea.highg = single(filtfilt(bpFilt, double(mea.Data)));
+	mea.highg(:, mea.BadChannels) = [];
+	
+	mea.X = mea.ElectrodeXY(:, 1);
+	mea.X(mea.BadChannels) = [];
+
+	mea.Y = mea.ElectrodeXY(:, 2);
+	mea.Y(mea.BadChannels) = [];
+end
+
+if ECOG
+	bpFilt = designfilt('bandpassfir','FilterOrder',150, ...
+		'CutoffFrequency1',2,'CutoffFrequency2',50, ...
+		'SampleRate', ecog.SamplingRate);
+	ecog.lfp = single(filtfilt(bpFilt, double(ecog.Data)));
+	ecog.lfp(:, ecog.BadChannels) = [];
+
+	bpFilt = designfilt('bandpassfir', 'FilterOrder', 150, ...
+		'CutoffFrequency1', 50, 'CutoffFrequency2', 124, ...  % 150 in the Schevon paper, but max is 124.93 given sampling freq
+		'SampleRate', ecog.SamplingRate);
+	ecog.highg = single(filtfilt(bpFilt, double(ecog.Data)));
+	ecog.highg(:, ecog.BadChannels) = [];
+	
+	ecog.X = ecog.ElectrodeXY(:, 1);
+	ecog.X(ecog.BadChannels) = [];
+	ecog.Y = ecog.ElectrodeXY(:, 2);
+	ecog.Y(ecog.BadChannels) = [];
+end
+
+
+
+
