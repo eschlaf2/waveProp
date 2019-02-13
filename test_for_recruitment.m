a46f91986402c79bcd9d9bb48f54cecb904e822e
Emily <eds2@bu.edu>
2019-02-13 12:29:44 -0500

    embed ff computation in function so that use can choose which method ('fano', 'wpli') to use for recruitment testing

--- test_for_recruitment.m
+++ test_for_recruitment.m
@@ -1,4 +1,4 @@
-function mea = test_for_recruitment(mea, PLOT)
+function mea = test_for_recruitment(mea, method, PLOT)
 % Test to see if the electrode array was recruited to the seizure as
 % described in ?Schevon, Catherine A., et al. ?Evidence of an Inhibitory
 % Restraint of Seizure Activity in Humans.? Nature Communications, vol. 3,
@@ -7,59 +7,98 @@ function mea = test_for_recruitment(mea, PLOT)
 % 
 % Uses weighted phase locking index
 
+METHOD_ALL = {'fano', 'wpli'};
+if ~exist('method', 'var') || isempty(method)
+	method = {'fano', 'wpli'};
+else
+	switch class(method)
+		case 'char'
+			if ~any(strcmp(METHOD_ALL, method))
+				error('%s not found in possible methods: %s', method, METHOD_ALL{:})
+			end
+		case 'cell'
+			for m = method(:)
+				if ~any(strcmp(METHOD_ALL, m))
+					error('%s not found in possible methods: %s', method, METHOD_ALL{:})
+				end
+			end
+		otherwise
+			error('Second input (method) should be of type ''char'' or ''cell''. Use [] to use default method.')
+	end
+		
+end
+
 if ~exist('PLOT', 'var')
 	PLOT = true;
 end
 
-%% Downsample lfp
-ds_freq = 1e3;  % Hz
-ds_step = floor(mea.SamplingRate / ds_freq);
-ds_freq = mea.SamplingRate / ds_step;
-lfp = mea.lfp(1:ds_step:end, :);
-T = mea.Time(1:ds_step:end);
-
-%% Set parameters for coherence calculations
-TW = 20;
-ntapers = 2*TW-1;
-params.Fs = ds_freq;
-params.tapers = [TW, ntapers];
-params.pad = 0;
-params.fpass = [2 30];
-params.err = [1 .005];
-
-%% Compute the multitaper complex cross spectrum at 160 ms intervals
-window = 1;  % length of window (s)
-winSamples = window * ds_freq;  % samples per window
-winstep = floor(160e-3 * ds_freq);  % step through the data in 160 ms increments
-nSamples = size(lfp, 1) - winSamples + 1;  % number of samples for which to compute coherence
-
-% initialize vector to store coherence
-wpli_mean = zeros(size(1:winstep:nSamples));  
-
-i = 0;  % increment by one
-for ti = 1:winstep:nSamples  % step through time at winstep increments
-	i = i + 1;
+%% Compute WPLI
+if any(strcmpi(method, 'wpli'))
 	
-	% Compute the complex cross spectrum
-	[Sc,~,~,~,~,f] = CrossSpecMatc(...
-		lfp(ti : ti + winSamples-1, :), window, params);
+	%% Downsample lfp
+	DOWNSAMPLED_FREQ = 1e3;  % Hz
+	ds_step = floor(mea.SamplingRate / DOWNSAMPLED_FREQ);
+	DOWNSAMPLED_FREQ = mea.SamplingRate / ds_step;
+	lfp = mea.lfp;
+	lfp = lfp(1:ds_step:end, :);
+	Time = mea.Time;
+	Time = Time(1:ds_step:end);
+
+	%% Set parameters for coherence calculations
+	W = 5;  % bandwidth (Hz)
+	T = 1;  % time of sample (s)
+	p = 1;  % ntapers = W*T - p
+
+
+	params.Fs = DOWNSAMPLED_FREQ;
+	params.tapers = [W T p];
+	params.pad = 0;  % padding factor for the FFT. if PAD = 0, we pad the FFT to the next power of 2
+	params.fpass = [2 30];  % frequency band to be used in the calculation 
+	% params.err = [1 .005];
+
+
+	%% Compute the multitaper complex cross spectrum at 160 ms intervals
+	window = 1;  % length of window (s)
+	winSamples = window * DOWNSAMPLED_FREQ;  % samples per window
+	winstep = floor(160e-3 * DOWNSAMPLED_FREQ);  % step through the data in 160 ms increments
+	nSamples = size(lfp, 1) - winSamples + 1;  % number of samples for which to compute coherence
+
+	% initialize vector to store coherence
+	wpli_mean = zeros(size(1:winstep:nSamples));  
+
+	i = 0;  % increment by one
+	for ti = 1:winstep:nSamples  % step through time at winstep increments
+		i = i + 1;
+
+		% Compute the complex cross spectrum and frequencies
+		[Sc,~,~,~,~,freq] = CrossSpecMatc(...
+			lfp(ti : ti + winSamples-1, :), window, params);
+
+		% Compute the weighted phase locking index for each channel pair
+		wpli = squeeze(abs(mean(imag(Sc), 1)) ./ mean(abs(imag(Sc)), 1));
+
+		% take the mean of the squared wpli values
+		wpli_mean(i) = mean(wpli(:).^2, 'omitnan');
+	end
 	
-	% Compute the weighted phase locking index for each channel pair
-	wpli = squeeze(abs(mean(imag(Sc), 1)) ./ mean(abs(imag(Sc)), 1));
-	
-	% take the mean of the squared wpli values
-	wpli_mean(i) = mean(wpli(:).^2, 'omitnan');
+	% Save results
+	disp('Saving WPLI')
+	mea.wpli = wpli_mean;
+	mea.wpliFreq = freq;
+
+	if PLOT
+		figure(4); clf; fullwidth()
+		plot(Time(1:winstep:ti), wpli_mean);
+		title([strrep(mea.Name, '_', ' '), ' Coherence'])
+		ylabel('Mean square WPLI')
+		xlabel('Time (s)')
+		axis tight
+	end
 end
-
-if PLOT
-	figure(4); fullwidth()
-	plot(T(1:winstep:ti), wpli_mean);
-	title([strrep(mea.Name, '_', ' '), ' Coherence'])
-	ylabel('Mean square WPLI')
-	xlabel('Time (s)')
-	axis tight
-end
 	
-mea.wpli = wpli_mean;
-mea.wpliF = f;
+
+%% Compute Fano factor
+if any(strcmpi(method, 'fano'))
+	mea = mua_fano_factor(mea);
+end
 
