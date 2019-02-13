0a6cb7f0ec166728675c537eadc6c24db36d0bfc
Emily <eds2@bu.edu>
2019-02-13 17:24:06 -0500

    Update smithSchevon2016_script.m
    Add BOS method for computing wave direction

--- smithSchevon2016_script.m
+++ smithSchevon2016_script.m
@@ -1,5 +1,5 @@
 disp('Loading data')
-m = matfile('MG49_Seizure43.mat');
+m = matfile('MG49_Seizure36.mat');
 filename = m.Name;
 outfile = [filename '_meaFilt'];
 
@@ -18,11 +18,11 @@ catch ME
 		case 'MATLAB:MatFile:VariableNotInFile'
 % 			errorLog = {errorLog; ME};
 			clear ME
-			try 
-				mea = matfile(outfile, 'writable', true);
-			catch ME				
+			if isempty(whos(outfile))
 				mea = m.Neuroport;
 % 				ecog = m.ECoG;
+			else
+				mea = matfile(outfile, 'writable', true);
 			end
 		otherwise
 			rethrow(ME)
@@ -102,6 +102,62 @@ print(2, nn, '-dpng')
 print(3, [nn '_FF'], '-dpng')
 print(4, [nn, '_coh'], '-dpng')
 
+%% Compute wave direction using BOS method
+
+% Downsample lfp
+position = [mea.X mea.Y];
+DS_FREQ = 1e3;  % downsampled frequency (Hz)
+DS_STEP = floor(mea.SamplingRate / DS_FREQ);  % how to step through the data
+DS_FREQ = mea.SamplingRate / DS_STEP;  % recompute DS_FREQ in case of rounding
+
+lfp = mea.lfp;  % import lfp band
+lfp = lfp(1 : DS_STEP : end, :);  % downsample
+NSAMP = size(lfp, 1);
+
+time = mea.Time;  % import time
+padding = mea.Padding;
+time = time(1 : DS_STEP : end);  % downsample
+
+
+BAND = [1 13];                  % Select a frequency range to analyze
+T = 2;							% Length of recording (s)
+W = 2;                          % Bandwidth
+ntapers = 2*(T * W) - 1;        % Choose the # of tapers.
+params.tapers = [T * W, ntapers];  % ... time-bandwidth product and tapers.
+params.Fs = DS_FREQ;                 % ... sampling rate
+params.pad = -1;                % ... no zero padding.
+params.fpass = BAND;            % ... freq range to pass
+params.err = [1 0.05];          % ... theoretical error bars, p=0.05.
+OVERLAP_COMPLEMENT = 1;         % T - OVERLAP (s)
+
+COMPUTE_INDS = [round(1 : OVERLAP_COMPLEMENT * DS_FREQ : NSAMP - T * DS_FREQ) NSAMP];
+TS = find(time(COMPUTE_INDS) > 0, 1);
+TE = find(time(COMPUTE_INDS) > (time(end) - padding(2)), 1) - 1;
+
+COMPUTE_INDS = COMPUTE_INDS(TS : TE - T);  % only compute while in seizure
+N = numel(COMPUTE_INDS);
+%%
+
+PLOT = ''; % 'plot';
+src_dir = zeros(N, 1);
+speed = zeros(N, 1);
+ci_dir = zeros(N, 2);
+ci_sp = zeros(N, 2);
+
+for i = 1:N
+	inds = COMPUTE_INDS(i) : (COMPUTE_INDS(i) + T * DS_FREQ - 1);
+	
+	fprintf('Estimating waves at time %d/%d\n', i, N)
+	[coh, phi, freq, coh_conf] = compute_coherence(lfp(inds, :), params);
+	
+	[delay, delay_ci_lo, delay_ci_up] = compute_delay(coh, coh_conf, phi, freq);
+	
+	[src_dir(i), speed(i), ci_dir(i, :), ci_sp(i, :)] = estimate_wave(delay, position, PLOT);
+
+end
+
+BOS = struct('src_dir', src_dir, 'speed', speed, 'ci_dir', ci_dir, 'ci_sp', ci_sp);
+mea.BOS = BOS;
 
 %% Define epochs and IDIs
 % The ictal wavefront epoch was defined as the time of the first channel?s mean 
