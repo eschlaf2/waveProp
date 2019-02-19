7030b7ba316602017bba62cbc5809f1700027ae1
Emily <eds2@bu.edu>
2019-02-18 19:38:09 -0500

    Update filter_mea_ecog.m
    More compatible with matfiles

--- filter_mea_ecog.m
+++ filter_mea_ecog.m
@@ -1,65 +1,105 @@
-function [mea, ecog] = filter_mea_ecog(mea, ecog)
+function [mea, ecog] = filter_mea_ecog(mea, ecog, outfile)
 
 
 MEA = exist('mea', 'var');
 ECOG = exist('ecog', 'var');
+if ~exist('outfile', 'var')
+	if MEA 
+		outfile = [mea.Name '_meaFilt'];
+	end
+	if ECOG
+		outfile = [ecog.Name '_ecogFilt'];
+	end
+end
+
 
 if MEA
+	data = mea.Data;
+	SamplingRate = mea.SamplingRate;
+	BadChannels = mea.BadChannels;
+	ElectrodeXY = mea.ElectrodeXY;
 
-	if isfield(mea, 'Position')  % deprecated
-		mea.ElectrodeXY = mea.Position;
-		rmfield(mea, 'Position')
+	if isstruct(mea)
+
+		if isfield(mea, 'Position')  % deprecated
+			mea.ElectrodeXY = mea.Position;
+			rmfield(mea, 'Position')
+		end
+
+		rmfield(mea, 'Data');
+		save(outfile, '-v7.3', '-struct', 'mea');
+		clear mea
+		mea = matfile(outfile, 'writable', true)
 	end
+
 
 	bpFilt = designfilt('bandpassfir','FilterOrder',150, ...
 		'CutoffFrequency1',2,'CutoffFrequency2',50, ...
-		'SampleRate', mea.SamplingRate);
+		'SampleRate', SamplingRate);
 
-	mea.lfp = single(filtfilt(bpFilt, double(mea.Data)));
-	mea.lfp(:, mea.BadChannels) = [];
+	temp = single(filtfilt(bpFilt, double(data)));
+	temp(:, BadChannels) = [];
+	mea.lfp = temp;
 	
 	bpFilt = designfilt('bandpassfir','FilterOrder',150, ...
 		'CutoffFrequency1',3e2,'CutoffFrequency2',3e3, ...
-		'SampleRate',mea.SamplingRate);
-	mea.mua = single(filtfilt(bpFilt, double(mea.Data)));
-	mea.mua(:, mea.BadChannels) = [];
+		'SampleRate',SamplingRate);
+	temp = single(filtfilt(bpFilt, double(data)));
+	temp(:, BadChannels) = [];
+	mea.mua = temp;
 
 	bpFilt = designfilt('bandpassfir', 'FilterOrder', 150, ...
 		'CutoffFrequency1', 50, 'CutoffFrequency2', 300, ...
-		'SampleRate', mea.SamplingRate);
-	mea.highg = single(filtfilt(bpFilt, double(mea.Data)));
-	mea.highg(:, mea.BadChannels) = [];
+		'SampleRate', SamplingRate);
+	temp = single(filtfilt(bpFilt, double(data)));
+	temp(:, BadChannels) = [];
+	mea.highg = temp;
 	
-	mea.X = mea.ElectrodeXY(:, 1);
-	mea.X(mea.BadChannels) = [];
+	mea.X = ElectrodeXY(:, 1);
+	mea.X(BadChannels) = [];
 
-	mea.Y = mea.ElectrodeXY(:, 2);
-	mea.Y(mea.BadChannels) = [];
+	mea.Y = ElectrodeXY(:, 2);
+	mea.Y(BadChannels) = [];
 end
 
 if ECOG
 
-	if isfield(ecog, 'Position')  % deprecated
-		ecog.ElectrodeXY = ecog.Position;
-		rmfield(ecog, 'Position')
+	data = ecog.Data;
+	SamplingRate = ecog.SamplingRate;
+	BadChannels = ecog.BadChannels;
+	ElectrodeXY = ecog.ElectrodeXY;
+
+	if isstruct(ecog)
+		
+		if isfield(ecog, 'Position')  % deprecated
+			ecog.ElectrodeXY = ecog.Position;
+			rmfield(ecog, 'Position')
+		end
+
+		rmfield(ecog, 'Data');
+		save(outfile, '-v7.3', '-struct', 'ecog');
+		clear ecog
+		ecog = matfile(outfile, 'writable', true)
 	end
 
 	bpFilt = designfilt('bandpassfir','FilterOrder',150, ...
 		'CutoffFrequency1',2,'CutoffFrequency2',50, ...
-		'SampleRate', ecog.SamplingRate);
-	ecog.lfp = single(filtfilt(bpFilt, double(ecog.Data)));
-	ecog.lfp(:, ecog.BadChannels) = [];
+		'SampleRate', SamplingRate);
+	temp = single(filtfilt(bpFilt, double(data)));
+	temp(:, BadChannels) = [];
+	ecog.lfp = temp
 
 	bpFilt = designfilt('bandpassfir', 'FilterOrder', 150, ...
 		'CutoffFrequency1', 50, 'CutoffFrequency2', 124, ...  % 150 in the Schevon paper, but max is 124.93 given sampling freq
-		'SampleRate', ecog.SamplingRate);
-	ecog.highg = single(filtfilt(bpFilt, double(ecog.Data)));
-	ecog.highg(:, ecog.BadChannels) = [];
+		'SampleRate', SamplingRate);
+	temp = single(filtfilt(bpFilt, double(data)));
+	temp(:, BadChannels) = [];
+	ecog.highg = temp;
 	
-	ecog.X = ecog.ElectrodeXY(:, 1);
-	ecog.X(ecog.BadChannels) = [];
-	ecog.Y = ecog.ElectrodeXY(:, 2);
-	ecog.Y(ecog.BadChannels) = [];
+	ecog.X = ElectrodeXY(:, 1);
+	ecog.X(BadChannels) = [];
+	ecog.Y = ElectrodeXY(:, 2);
+	ecog.Y(BadChannels) = [];
 end
 
 
