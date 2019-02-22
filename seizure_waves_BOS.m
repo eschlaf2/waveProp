7e9939367ddc89755902f9d241dfb48a59f61b99
Emily <eds2@bu.edu>
2019-02-22 15:45:52 -0500

    Update seizure_waves_BOS.m

--- seizure_waves_BOS.m
+++ seizure_waves_BOS.m
@@ -2,86 +2,69 @@ function [mea] = seizure_waves_BOS(mea, PLOT)
 
 %% Parse input
 if ~exist('PLOT', 'var')
-	PLOT = true;
+	if ~feature('ShowFigureWindows')
+		PLOT = true;
+	else
+		PLOT = false;
+	end
 end
 
 % data = mea.lfp;
 position = [mea.X mea.Y];
 
-%% Compute coherence and phase
+%% Set parameters
 % Note: the chronux toolbox must be on the path
 
-disp('Computing coherence and phase')
+% Downsample lfp
+DS_FREQ = 1e3;  % downsampled frequency (Hz)
+DS_STEP = floor(mea.SamplingRate / DS_FREQ);  % how to step through the data
+DS_FREQ = mea.SamplingRate / DS_STEP;  % recompute DS_FREQ in case of rounding
 
-if strcmp(which('chronux'), '') % required toolbox
-	error(['Chronux toolbox not found. ' ...
-		'Add the toolbox to your matlab path using ' ...
-		'`addpath(genpath(path_to_chronux))`.']);
-end
+lfp = mea.lfp;  % import lfp band
+lfp = lfp(1 : DS_STEP : end, :);  % downsample
+NSAMP = size(lfp, 1);
+
+time = mea.Time;  % import time
+padding = mea.Padding;
+time = time(1 : DS_STEP : end);  % downsample
+
 
 BAND = [1 13];                  % Select a frequency range to analyze
 T = 2;							% Length of recording (s)
 W = 2;                          % Bandwidth
 ntapers = 2*(T * W) - 1;        % Choose the # of tapers.
 params.tapers = [T * W, ntapers];  % ... time-bandwidth product and tapers.
-params.Fs = mea.SamplingRate / 30;                 % ... sampling rate
+params.Fs = DS_FREQ;                 % ... sampling rate
 params.pad = -1;                % ... no zero padding.
 params.fpass = BAND;            % ... freq range to pass
 params.err = [1 0.05];          % ... theoretical error bars, p=0.05.
+OVERLAP_COMPLEMENT = 1;         % T - OVERLAP (s)
 
-[coh, phi, freq, coh_conf] = compute_coherence(mea.lfp(1:30:(2*3e4), :), params);
-% save(['../data/example_' data_set '_coherence'], 'coh', 'phi', 'freq', 'coh_conf');
+COMPUTE_INDS = [round(1 : OVERLAP_COMPLEMENT * DS_FREQ : NSAMP - T * DS_FREQ) NSAMP];
+TS = find(time(COMPUTE_INDS) > 0, 1);
+TE = find(time(COMPUTE_INDS) > (time(end) - padding(2)), 1) - 1;
 
-if PLOT
-	figure;
-	plot(freq, squeeze(coh(1,8,:)));
-	xlabel('Frequency (Hz)');
-	ylabel('Coherence');
-
-	figure;
-	subplot(1,2,1)
-	imagesc(squeeze(coh(:,:, find(freq >= 6, 1))));
-	c = colorbar;
-	c.Label.String = 'Coherence at 6 Hz';
-	xlabel('Electrodes');
-	ylabel('Electrodes');
-	subplot(1,2,2)
-	imagesc(squeeze(phi(:,:, find(freq >= 6, 1))));
-	c = colorbar;
-	c.Label.String = 'Phase at 6 Hz';
-	xlabel('Electrodes');
-	ylabel('Electrodes');
-end
-
-%% Estimate delays between electrodes
-
-disp('Estimating delays between electrodes')
-
-[delay, delay_ci_lo, delay_ci_up] = compute_delay(coh, coh_conf, phi, freq);
-
+COMPUTE_INDS = COMPUTE_INDS(TS : TE - T);  % only compute while in seizure
+N = numel(COMPUTE_INDS);
 %%
-if PLOT
-	figure;
-	imagesc(1000 * delay);
-	c = colorbar;
-	c.Label.String = 'Delay (ms)';
-	xlabel('Electrodes');
-	ylabel('Electrodes');
 
-	% find center electrode
-	[~, center] = min((position(:,1) - mean(position(:,1))).^2 + (position(:,2) - mean(position(:,2))).^2);
+src_dir = zeros(N, 1);
+speed = zeros(N, 1);
+ci_dir = zeros(N, 2);
+ci_sp = zeros(N, 2);
 
-	figure;
-	scatter(position(:,1), position(:,2), 400, 1000 * delay(center,:), 'filled');
-	hold on
-	plot(position(center,1), position(center,2), 'X');
-	c = colorbar;
-	c.Label.String = 'Delay to center electrode X (ms)';
-	xlabel('Electrode position (mm)');
-	ylabel('Electrodes position (mm)');
+for i = 1:N
+	inds = COMPUTE_INDS(i) : (COMPUTE_INDS(i) + T * DS_FREQ - 1);
+	
+	fprintf('Estimating waves at time %d/%d\n', i, N)
+	[coh, phi, freq, coh_conf] = compute_coherence(lfp(inds, :), params);
+	
+	[delay, delay_ci_lo, delay_ci_up] = compute_delay(coh, coh_conf, phi, freq);
+	
+	[src_dir(i), speed(i), ci_dir(i, :), ci_sp(i, :)] = estimate_wave(delay, position, '');
+
 end
 
-
-%% Estimate parameters of the waves
-[src_dir, speed, ci_dir, ci_sp] = estimate_wave(delay, position, 'plot');
+BOS = struct('src_dir', src_dir, 'speed', speed, 'ci_dir', ci_dir, 'ci_sp', ci_sp);
+mea.BOS = BOS;
 
