dc08c599aeb11b89165f9f660b6b86d093a88d42
Emily <eds2@bu.edu>
2019-05-06 19:45:09 -0400

    Update vid_50_60.m

--- scripts/vid_50_60.m
+++ scripts/vid_50_60.m
@@ -4,7 +4,25 @@
 %     mea = matfile('c5/c5_Seizure1_Neuroport.mat');
 %     electrode_vid_full;
 
-mea = load('c5/c5_Seizure1_Neuroport_10_10.mat');  % load data
+patpath = genpath(pat);
+addpath(patpath);
+
+fname = sprintf('%s_Seizure%d_Neuroport', pat, seizure);
+if ~exist([fname '.mat'], 'file')
+	if exist([fname '_10_10.mat'], 'file')
+		fname = [fname '_10_10.mat'];
+		fprintf('Using %s\n', fname);
+	elseif exist(sprintf('%s_Seizure%d.mat', pat, seizure), 'file')
+		disp('Creating epoch file ...')
+		create_epoch(pat, seizure, 'padding', [10 10]);
+		fprintf('Using %s\n', fname);
+		fname = [fname '_10_10.mat'];
+	end
+end
+
+mea = load(fname);
+
+% mea = load('c5/c5_Seizure1_Neuroport_10_10.mat');  % load data
 mea = exclude_channels(mea);  % remove non-participating channels
 P = mea.Position;  % get electrode coordinates
 P(mea.BadChannels, :) = [];  % ... and remove bad channels
@@ -20,15 +38,20 @@ time = time(inds_50_60);  % ... same
 [~, mea] = filter_mea(mea, [], {'lfp'; 'mua'});  % filter the raw data
 event_inds = mua_events(mea);  % compute event times
 
+[nT, nCh] = size(mea.mua);
+ds = round(abs(diff(time(1:2))) * mea.SamplingRate);  % compute the binwidth
+fr_high = false([nT, nCh]);
+fr_high(event_inds) = true;
+fr_high = fr_high(1:(floor(nT / ds) * ds), :);  % trim
+fr_high = squeeze(sum(reshape(fr_high, ds, [], nCh)));
+
 inds_lfp = interp1(downsample(Time, mea.skipfactor), ...  % Select subset of lfp corresponding to 50-60 s
 	1:length(mea.lfp), time, 'nearest');  
-	
-ds = abs(diff(time(1:2))) * mea.SamplingRate;  % compute the binwidth
-bincounts = histcounts(event_inds, (1:ds:prod(size(mea.mua))) + ds/2);  % ... and the counts per bin
-fr_high = reshape(bincounts, [], size(mea.mua, 2));  % Convert to array
+inds_fr = interp1(Time((1:length(fr_high)) * ds), ...
+	1:length(fr_high), time, 'nearest');
 
 close all;
 framerate = 100;
-electrode_vid({fr; fr_high(inds_50_60, :); mea.lfp(inds_lfp, :)}, ...
+electrode_vid({fr; fr_high(inds_fr, :); mea.lfp(inds_lfp, :)}, ...
 	P(:, 1), P(:, 2), time, time(end), framerate, ...
 	mea.Name, true)
