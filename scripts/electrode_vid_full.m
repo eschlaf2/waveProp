84bfd80a2c0259cbb56b2529a22099a779d6790b
Emily <eds2@bu.edu>
2019-05-07 13:39:11 -0400

    Update electrode_vid_full.m

--- scripts/electrode_vid_full.m
+++ scripts/electrode_vid_full.m
@@ -4,41 +4,29 @@
 %     mea = matfile('c5/c5_Seizure1_Neuroport.mat');
 %     electrode_vid_full;
 
-mea = exclude_channels(mea);
+mea = exclude_channels(mea);  % Exclude channels based on activity
 
-dsRate = 100;
-[fr, time] = lowpass_filt_firingRate(mea);
-try
-	lfp = mea.lfp;
-catch 
-	lfp = filter_mea(mea, [], {'lfp'});
-	skipfactor = lfp.skipfactor;
-	lfp = lfp.lfp;
-end
-try
-	mua = mea.mua;
-catch 
-	mua = filter_mea(mea, [], {'mua'});
-	mua = mua.mua;
-end
+dsRate = 100;  % Set the desired downsampling rate for the video (frameRate is half of this... below)
+[fr, time] = lowpass_filt_firingRate(mea);  % create the data for the left plot
+[~, mea] = filter_mea(mea, {'lfp'; 'mua'});  % filter the raw data
 
-Time = mea.Time;
+Time = mea.Time;  % Get relevant time points
 Time = Time();
 te = Time(end) - mea.Padding(1, 2);
-inds = arrayfun(@(t) find(Time == t, 1), time);
-	
-fr_high = zeros(size(mua));
-if any(strcmpi(fieldnames(mea), 'event_inds'))
-	event_inds = mea.event_inds;
-else
-	event_inds = mua_events(mea);
-end
 t0 = find(Time == 0, 1);
-event_inds(event_inds < t0) = [];
+inds = arrayfun(@(t) find(Time == t, 1), time);
+
+P = mea.Position;  % Get electrode coordinates
+P(mea.BadChannels, :) = [];  % remove excluded channels
+	
+% Get MUA activity
+fr_high = zeros(size(mua));
+event_inds = mua_events(mea);
+
 fr_high(event_inds) = 1;
 ds = inds(2) - inds(1);
 fr_high = smoothdata(fr_high, 1, 'movmean', ds) * ds;
 close all;
 electrode_vid({fr; fr_high(inds, :); lfp(round(inds/skipfactor) + 1, :)}, ...
-	mea.X, mea.Y, time, time(end), dsRate / 2, ...
+	P(:, 1), P(:, 2), time, time(end), dsRate / 2, ...
 	mea.Name, true)
