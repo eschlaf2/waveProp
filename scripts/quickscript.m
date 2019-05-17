b6743e292d8280b261f21cf0bdb41ddeec20796d
Emily <eds2@bu.edu>
2019-05-17 13:31:13 -0400

    Update quickscript.m

--- scripts/quickscript.m
+++ scripts/quickscript.m
@@ -1,23 +1,23 @@
 % filename = m.Name;
 % mea = m.Neuroport;
 % pat = 'c5'; seizure = 3;
-patpath = genpath(pat);
-addpath(patpath);
-
-fname = sprintf('%s_Seizure%d_Neuroport', pat, seizure);
-if ~exist([fname '.mat'], 'file')
-	if exist([fname '_10_10.mat'], 'file')
-		fname = [fname '_10_10.mat'];
-		fprintf('Using %s\n', fname);
-	elseif exist(sprintf('%s_Seizure%d.mat', pat, seizure), 'file')
-		disp('Creating epoch file ...')
-		create_epoch(pat, seizure, 'padding', [10 10]);
-		fprintf('Using %s\n', fname);
-		fname = [fname '_10_10.mat'];
-	end
-end
-mea = matfile(fname);
-[~, name, ~] = fileparts(mea.Properties.Source);
+% patpath = genpath(pat);
+% addpath(patpath);
+% 
+% fname = sprintf('%s_Seizure%d_Neuroport', pat, seizure);
+% if ~exist([fname '.mat'], 'file')
+% 	if exist([fname '_10_10.mat'], 'file')
+% 		fname = [fname '_10_10.mat'];
+% 		fprintf('Using %s\n', fname);
+% 	elseif exist(sprintf('%s_Seizure%d.mat', pat, seizure), 'file')
+% 		disp('Creating epoch file ...')
+% 		create_epoch(pat, seizure, 'padding', [10 10]);
+% 		fprintf('Using %s\n', fname);
+% 		fname = [fname '_10_10.mat'];
+% 	end
+% end
+% mea = matfile(fname);
+% [~, name, ~] = fileparts(mea.Properties.Source);
 
 outfile = matfile([name '_wave_prop'], 'writable', true);
 
