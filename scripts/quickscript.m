98671e997fc048d48b97a472a2772d97ab301575
Emily <eds2@bu.edu>
2019-05-20 09:00:39 -0400

    Update quickscript.m

--- scripts/quickscript.m
+++ scripts/quickscript.m
@@ -1,33 +1,33 @@
 % filename = m.Name;
 % mea = m.Neuroport;
 % pat = 'c5'; seizure = 3;
-% patpath = genpath(pat);
-% addpath(patpath);
-% 
-% fname = sprintf('%s_Seizure%d_Neuroport', pat, seizure);
-% if ~exist([fname '.mat'], 'file')
-% 	if exist([fname '_10_10.mat'], 'file')
-% 		fname = [fname '_10_10.mat'];
-% 		fprintf('Using %s\n', fname);
-% 	elseif exist(sprintf('%s_Seizure%d.mat', pat, seizure), 'file')
-% 		disp('Creating epoch file ...')
-% 		create_epoch(pat, seizure, 'padding', [10 10]);
-% 		fprintf('Using %s\n', fname);
-% 		fname = [fname '_10_10.mat'];
-% 	end
-% end
-% mea = matfile(fname);
-% [~, name, ~] = fileparts(mea.Properties.Source);
+patpath = genpath(pat);
+addpath(patpath);
 
-mea = load('SIM/seizing_cortical_field_sim.mat');
-name = mea.Name;
+fname = sprintf('%s_Seizure%d_Neuroport', pat, seizure);
+if ~exist([fname '.mat'], 'file')
+	if exist([fname '_10_10.mat'], 'file')
+		fname = [fname '_10_10.mat'];
+		fprintf('Using %s\n', fname);
+	elseif exist(sprintf('%s_Seizure%d.mat', pat, seizure), 'file')
+		disp('Creating epoch file ...')
+		create_epoch(pat, seizure, 'padding', [10 10]);
+		fprintf('Using %s\n', fname);
+		fname = [fname '_10_10.mat'];
+	end
+end
+mea = load(fname);
+[~, name, ~] = fileparts(fname);
+
+% mea = load('SIM/seizing_cortical_field_sim.mat');
+% name = mea.Name;
 outfile = matfile([name '_wave_prop'], 'writable', true);
 
-% disp('Computing wave directions from delays ...')
-% [delays, mea] = wave_prop(mea, 'delays');
-% plot_wave_directions(mea, delays);
-% print(gcf, delays.Name, '-dpng')
-% outfile.delays = delays;
+disp('Computing wave directions from delays ...')
+[delays, mea] = wave_prop(mea, 'delays');
+plot_wave_directions(mea, delays);
+print(gcf, delays.Name, '-dpng')
+outfile.delays = delays;
 
 disp('Computing wave directions from events ...')
 [events, mea] = wave_prop(mea, 'events');
