6bcedfb78b605de587f7724d3c3b44a509862ce7
Emily <eds2@bu.edu>
2019-05-28 16:20:58 -0400

    Update wave_prop.m

--- wave_prop.m
+++ wave_prop.m
@@ -117,7 +117,7 @@ switch metric
 		[lfp, skipfactor, mea] = get_lfp(mea);
 		Time = downsample(Time, skipfactor);
 		TimeMs = Time * 1e3;
-		compute_inds = arrayfun(@(t) find(t == TimeMs, 1), computeTimes);
+		compute_inds = arrayfun(@(t) find(t >= TimeMs, 1), computeTimes);
 		[params, ~] = set_coherence_params(mea, Time, T);
 		plotTitles = strrep(mea.Name, '_', ' ');
 % 		computeTimes = TimeMs(compute_inds);
