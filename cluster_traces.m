e1b8b3829731ce4cb604da463e5d795dcd21bb8b
Emily <eds2@bu.edu>
2019-05-28 20:35:48 -0400

    Update cluster_traces.m

--- cluster_traces.m
+++ cluster_traces.m
@@ -8,3 +8,5 @@ subplot(2,2,1); plot(C'/10 + (1:kN));
 dist = pdist(C);
 subplot(2,2,2); imagesc(squareform(dist));
 subplot(223); stem(sum(squareform(dist)))
+subplot(224); colormap(hsv(kN + 1));
+imagesc(sparse(position(:, 1), position(:, 2), idx));
