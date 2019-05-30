c4d4934d1565dae805521095a12f03eca17aae6d
Emily <eds2@bu.edu>
2019-05-30 16:13:27 -0400

    Update preproc_data.m

--- scripts/preproc_data.m
+++ scripts/preproc_data.m
@@ -1,8 +1,11 @@
 % function preproc_data(mea)
 
-data = double(mea.Data);
-[nT, nCh] = size(data);
-[coeff, score, ~, ~, explained, ~] = pca(data);
+raw = double(mea.Data);
+[nT, nCh] = size(raw);
+badCh = find(arrayfun(@(ii) numel(unique(raw(:, ii))), 1:nCh) < nT * 1e-3);  % if the number of unique readings is less than 0.1% of the total number of readings, exclude the channel
+goodCh = 1:nCh; goodCh(badCh) = [];
+[coeff, score, ~, ~, explained, ~] = pca(raw(:, goodCh));
+nPCs = size(coeff, 2);
 
 %% Figure 10: raw v. preprocessed
 figure(10); clf; fullwidth(1)
@@ -17,35 +20,33 @@ title('Variance explained (Raw)')
 xlabel('Component'); ylabel('Percent explained')
 
 subplot(r, c, 2)  % Standard deviation of raw traces
-stem(std(data))
+stem(std(raw(:, goodCh)))
 title('Standard deviation (Raw)')
 xlabel('Channel'); ylabel('STD');
 
 %% Preprocessing
 % Use PCA to separate neural signal from electrode drift(?) 
-hiVar = find(abs(zscore(std(data))) > 1);  % Channels with high variance drown out neural signal
+hiVar = find(abs(zscore(std(raw))) > 1);  % Channels with high variance drown out neural signal
 [mCh, singleCh] = find(abs(coeff) > .6);  % Find PCs that describe the activity of a single channel
 badPCs = singleCh(any(hiVar' == mCh'));  % Filter to channels with high variance and big PC contributions (large changes in electrode reading that are probably not biological)
-badPCs = union(badPCs, find(explained > 60));
-PCs = 1:nCh; PCs(badPCs) = [];  % Exclude these PCs
-data = score(:, PCs) * coeff(:, PCs)';  % ... and rebuild the data
+% badPCs = union(badPCs, find(explained > 60));
+PCs = 1:nPCs; PCs(badPCs) = [];  % Exclude these PCs
+data = raw;
+data(:, goodCh) = score(:, PCs) * coeff(:, PCs)';  % ... and rebuild the data
 
 %% Figure 13: Scores
 figure(13); clf; fullwidth(true)
 cmap = lines;
-goodPCs = 1:nCh; goodPCs(badPCs) = [];
+goodPCs = 1:nPCs; goodPCs(badPCs) = [];
 plot(zscore(score(:, goodPCs))/10 + goodPCs, 'color', cmap(1, :)); hold on;
 plot(zscore(score(:, badPCs))/10 + badPCs', 'color', cmap(2, :)); hold off;
 ylim([0 nCh+1])
 
-%% Rerun PCA
-[coeff, score, latent, ~, explained, ~] = pca(data);  % Rerun PCA
-
-%% Figure 11: PCs of preprocessed data
+%% Figure 11: Show first 20 PCs
 figure(11); clf; fullwidth(1);
 for ii = 1:19
 	subplot(4, 5, ii)
-	imagesc(sparse(mea.Position(:, 1), mea.Position(:, 2), coeff(:, ii)))
+	imagesc(sparse(mea.Position(goodCh, 1), mea.Position(goodCh, 2), coeff(:, ii)))
 	axis square
 	title(sprintf('PC%d: %0.2f%%', ii, explained(ii)));
 end
@@ -61,28 +62,33 @@ annotation(figure(11),'textbox',...
 	'HorizontalAlignment', 'center', ...
     'FitBoxToText','on');
 
+%% Rerun PCA
+% [coeff, score, latent, ~, explained, ~] = pca(data(:, goodCh));  % Rerun PCA
+explainedPreproc = explained(goodPCs);
+explainedPreproc = explainedPreproc / sum(explainedPreproc);
+
 %% Figure 10: Raw v. pre-processed (cont)
 figure(10);
 subplot(r, c, 3)  % Variance explained by each PC (preproc'd data)
-stem(cumsum(explained(1:20)));
+stem(cumsum(explainedPreproc(1:20)));
 hold on;
-stem(explained(1:20))
+stem(explainedPreproc(1:20))
 hold off;
 title('Variance explained (Preproc)')
 xlabel('Component'); ylabel('Percent explained')
 
 subplot(r, c, 4)  % STD on each channel (preproc'd data)
-stem(std(data))
+stem(std(data(:, goodCh)))
 title('Standard deviation (Preproc)')
 xlabel('Channel'); ylabel('STD');
 
 %% Figure 12: Remove channels with extreme deviations
 figure(12); clf; fullwidth(true)
 cmap = lines;
-badCh = union(find(abs(zscore(std(data))) > 2), ...  % Channels with extreme variance should be excluded
-	find(std(data) < 1));  % ... as well as inactive channels
+badCh = union(goodCh(zscore(std(data(:, goodCh))) < -2.5), badCh);  % Channels with low variance should be excluded
 goodCh = 1:nCh; goodCh(badCh) = [];
-plot(zscore(data(:, goodCh))/10 + goodCh, 'Color', cmap(1, :)); hold on;  % Plot good channels
+plot(zscore(raw(:, goodCh)) ./ 10 + goodCh, 'Color', cmap(1, :)); hold on;  % Plot raw data
+plot(zscore(data(:, goodCh))/10 + goodCh, 'Color', cmap(3, :)); hold on;  % ... and good channels
 plot(zscore(data(:, badCh))/10 + badCh, 'Color', cmap(2, :)); hold off  % ... and bad channels
 title({'Bad Channels:'; num2str(badCh)})
 axis('tight')
