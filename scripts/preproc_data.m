054aa7e9d062e64058b164e3e5b5e5d925dbfc47
Emily <eds2@bu.edu>
2019-05-30 18:23:22 -0400

    Update preproc_data.m

--- scripts/preproc_data.m
+++ scripts/preproc_data.m
@@ -7,28 +7,6 @@ goodCh = 1:nCh; goodCh(badCh) = [];
 [coeff, score, ~, ~, explained, ~] = pca(raw(:, goodCh));
 nPCs = size(coeff, 2);
 
-%% Figure 10: raw v. preprocessed
-figure(10); clf; fullwidth(1)
-[r, c] = deal(2);
-
-subplot(r, c, 1)  % Variance explained by PCs in raw data
-stem(cumsum(explained(1:20)));
-hold on;
-stem(explained(1:20))
-hold off;
-title('Variance explained (Raw)')
-xlabel('Component'); ylabel('Percent explained')
-
-subplot(r, c, 2)  % Standard deviation of raw traces
-yyaxis left
-stem(std(raw(:, goodCh)))
-ylabel('STD');
-yyaxis right
-stem(zscore(std(raw(:, goodCh))));
-ylabel('STD (normed)')
-title('Standard deviation (Raw)')
-xlabel('Channel'); 
-
 %% Preprocessing
 % Use PCA to separate neural signal from electrode drift(?) 
 hiVar = find(abs(zscore(std(raw))) > 1);  % Channels with high variance drown out neural signal
@@ -44,7 +22,10 @@ figure(13); clf; fullwidth(true)
 cmap = lines;
 plot(zscore(score(:, PCs))/10 + PCs, 'color', cmap(1, :)); hold on;
 plot(zscore(score(:, badPCs))/10 + badPCs', 'color', cmap(2, :)); hold off;
+axis tight;
+xlabel('Time (samples)'); ylabel('PC');
 ylim([0 nCh+1])
+title({[mea.Name ' Bad PCs:']; num2str(badPCs)})
 
 %% Figure 11: Show first 20 PCs
 figure(11); clf; fullwidth(1);
@@ -68,10 +49,32 @@ annotation(figure(11),'textbox',...
 
 %% Rerun PCA
 % [coeff, score, latent, ~, explained, ~] = pca(data(:, goodCh));  % Rerun PCA
-explainedPreproc = explained(goodPCs);
+explainedPreproc = explained(PCs);
 explainedPreproc = explainedPreproc / sum(explainedPreproc);
 
-%% Figure 10: Raw v. pre-processed (cont)
+%% Figure 10: Raw v. preprocessed
+
+figure(10); clf; fullwidth(1)
+[r, c] = deal(2);
+
+subplot(r, c, 1)  % Variance explained by PCs in raw data
+stem(cumsum(explained(1:20)));
+hold on;
+stem(explained(1:20))
+hold off;
+title([strrep(mea.Name, '_', ' ') 'Variance explained (Raw)'])
+xlabel('Component'); ylabel('Percent explained')
+
+subplot(r, c, 2)  % Standard deviation of raw traces
+yyaxis left
+stem(goodCh, std(raw(:, goodCh)))
+ylabel('STD');
+yyaxis right
+stem(goodCh, zscore(std(raw(:, goodCh))));
+ylabel('STD (normed)')
+title('Standard deviation (Raw)')
+xlabel('Channel'); 
+
 figure(10);
 subplot(r, c, 3)  % Variance explained by each PC (preproc'd data)
 stem(cumsum(explainedPreproc(1:20)));
@@ -83,10 +86,10 @@ xlabel('Component'); ylabel('Percent explained')
 
 subplot(r, c, 4)  % STD on each channel (preproc'd data)
 yyaxis left
-stem(std(data(:, goodCh)))
+stem(goodCh, std(data(:, goodCh)))
 ylabel('STD');
 yyaxis right
-stem(zscore(std(data(:, goodCh))));
+stem(goodCh, zscore(std(data(:, goodCh))));
 ylabel('STD (normed)')
 title('Standard deviation (Preproc)')
 xlabel('Channel'); ylabel('STD');
@@ -94,12 +97,12 @@ xlabel('Channel'); ylabel('STD');
 %% Figure 12: Remove channels with extreme deviations
 figure(12); clf; fullwidth(true)
 cmap = lines;
-badCh = union(goodCh(zscore(std(data(:, goodCh))) < -2.5), badCh);  % Channels with low variance should be excluded
+badCh = union(goodCh(zscore(std(data(:, goodCh))) < -2), badCh);  % Channels with low variance should be excluded
 goodCh = 1:nCh; goodCh(badCh) = [];
-plot(zscore(raw(:, goodCh)) ./ 10 + goodCh, 'Color', cmap(1, :)); hold on;  % Plot raw data
+plot(zscore(double(mea.Data(:, goodCh))) ./ 10 + goodCh, 'Color', cmap(1, :)); hold on;  % Plot raw data
 plot(zscore(data(:, goodCh))/10 + goodCh, 'Color', cmap(4, :)); hold on;  % ... and good channels
 plot(zscore(data(:, badCh))/10 + badCh, 'Color', cmap(2, :)); hold off  % ... and bad channels
-title({'Bad Channels:'; num2str(badCh)})
+title({[strrep(mea.Name, '_', ' ') ' Bad Channels:']; num2str(badCh)})
 axis('tight')
 ylim([0 nCh+1])
 
