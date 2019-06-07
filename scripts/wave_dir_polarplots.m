1bf837b79ae00134026e81c93527d675738d35d5
Emily <eds2@bu.edu>
2019-06-06 20:12:54 -0400

    Update wave_dir_polarplots.m

--- scripts/wave_dir_polarplots.m
+++ scripts/wave_dir_polarplots.m
@@ -5,15 +5,15 @@ nF = numel(files);
 clear res;
 res(nF) = struct('name', [], 'data', [], 'Z', [], 'time', [], 'Vx', 'Vy');
 figure(); fullwidth(true);
-whichvars = [2 3 4];
+whichfields = [2 3 4];
 for ii = 1:nF
 	res(ii).name = files(ii).name(strfind(files(ii).name, 'Seizure')+(7:8));
 	res(ii).data = load(fullfile(files(ii).folder, files(ii).name));
 	fields = fieldnames(res(ii).data);
-	res(ii).time = res(ii).data.(fields{1}).computeTimes / 1e3;
+	res(ii).time = res(ii).data.(fields{whichfields(1)}).computeTimes / 1e3;
 	[res(ii).Z, res(ii).Vx, res(ii).Vy] = ...
-		deal(zeros(length(res(ii).data.(fields{1}).computeTimes), length(fields)));
-	for jj = 1:length(fields)
+		deal(zeros(length(res(ii).data.(fields{whichfields(1)}).computeTimes), length(fields)));
+	for jj = whichfields
 		for f = {'Z', 'Vx', 'Vy'}
 			f = f{1};
 			switch f
@@ -28,31 +28,31 @@ for ii = 1:nF
 			res(ii).(f)(res(ii).data.(fields{jj}).p > .05, :) = nan;
 		end
 	end
-% 	ax(ii) = polaraxes();
-	ax(ii) = axes();
+	ax(ii) = polaraxes();
+% 	ax(ii) = axes();
 	subplot(2, nF, ii, ax(ii));
 	tt = linspace(res(ii).time(1), res(ii).time(end), length(res(ii).time) * 10);
 % 	data = interp1(res(ii).time, smoothdata(unwrap(res(ii).Z(:, whichvars)), 'movmean', 20), tt);
 	data = smoothdata(...
-		interp1(res(ii).time, unwrap(res(ii).Z(:, whichvars)), tt), ...
+		interp1(res(ii).time, unwrap(res(ii).Z(:, whichfields)), tt), ...
 		'movmean', 100);
-% 	polarplot(ax(ii), data, tt, '-', 'linewidth', 2);
-	plot3(ax(ii), cos(data), sin(data), tt); hold on
-	ax(ii).ColorOrderIndex = 1;
-	plot3(ax(ii), cos(res(ii).Z(:, whichvars)), ...
-		sin(res(ii).Z(:, whichvars)), res(ii).time, '.', 'markersize', 10); hold off
+	polarplot(ax(ii), data, tt, '-', 'linewidth', 2);
+% % 	plot3(ax(ii), cos(data), sin(data), tt); hold on
+% 	ax(ii).ColorOrderIndex = 1;
+% 	plot3(ax(ii), cos(res(ii).Z(:, whichfields)), ...
+% 		sin(res(ii).Z(:, whichfields)), res(ii).time, '.', 'markersize', 10); hold off
 % 	title([pat ' ' res(ii).name]);
 	axis tight;
 % 	hold on;
 % 	set(gca, 'ColorOrderIndex', 1)
 	ax(ii + nF) = polaraxes();
 	subplot(2, nF, ii + nF, ax(ii + nF));
-	polarplot(ax(ii + nF), res(ii).Z(:, whichvars), res(ii).time, '.', ...
+	polarplot(ax(ii + nF), res(ii).Z(:, whichfields), res(ii).time, '.', ...
 		'markersize', 10);
 	hold off;
 	axis tight
 end
-legend(fields(whichvars), 'position', [.9 .55 0 0])
+legend(fields(whichfields), 'position', [.9 .55 0 0])
 rlim = arrayfun(@(a) a.RLim(2), ax);
 for ii = 1:numel(ax)
 	ax(ii).RLim = [0 max(rlim)];
