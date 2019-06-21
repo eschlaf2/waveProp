736979b4b6e141c615e5f13691fef52e2c952418
Emily <eds2@bu.edu>
2019-06-20 22:09:34 -0400

    Update quickscript.m

--- scripts/quickscript.m
+++ scripts/quickscript.m
@@ -24,28 +24,28 @@ end
 % mea = load('SIM/seizing_cortical_field_sim.mat');
 % name = mea.Name;
 outfile = matfile([name '_wave_prop_all_waves'], 'writable', true);
+mea = exclude_channels(mea);
+[~, mea] = get_discharge_times(mea, 'method', 2);
 
 disp('Computing wave directions from events ...')
-[events, mea] = wave_prop(mea, 'events');
+[events, mea] = wave_prop(mea, 'events', 'exclude', false);
 plot_wave_directions(mea, events);
 print(gcf, events.Name, '-dpng');
 outfile.events = events;
 
 disp('Computing wave directions from maxdescent ...')
-[maxdescent, mea] = wave_prop(mea, 'maxdescent');
+[maxdescent, mea] = wave_prop(mea, 'maxdescent', 'exclude', false);
 plot_wave_directions(mea, maxdescent);
 print(gcf, maxdescent.Name, '-dpng');
 outfile.maxdescent = maxdescent;
 
 disp('Computing wave directions from rising deviance ...')
-[rising, mea] = wave_prop(mea, 'rising');
+[rising, mea] = wave_prop(mea, 'rising', 'exclude', false);
 plot_wave_directions(mea, rising);
 print(gcf, rising.Name, '-dpng');
 outfile.rising = rising;
 
 disp('Computing wave directions from falling deviance ...')
-mea = exclude_channels(mea);
-[~, mea] = get_discharge_times(mea, 'method', 2);
 [falling, mea] = wave_prop(mea, 'falling', 'thresh', -Inf, 'exclude', false);
 plot_wave_directions(mea, falling);
 print(gcf, falling.Name, '-dpng');
