2c8055dfda877761659c3aaea9b74fc2bdd001e4
eschlaf2 <eschlaf2@illinois.edu>
2019-06-21 18:45:35 -0400

    update wave_dir_polarplots

--- scripts/wave_dir_polarplots.m
+++ scripts/wave_dir_polarplots.m
@@ -1,6 +1,7 @@
 % pat = 'MG49';
 files = dir([pat '_Seizure*wave_prop.mat']);
 nF = numel(files);
+sig = 5e-2;
 
 clear res;
 res(nF) = struct(...
@@ -12,13 +13,14 @@ res(nF) = struct(...
     'Vy', []);
 
 figure(1); clf; fullwidth(true);
-whichfields = [1:3 5:6]; %[2 3 4];
-whichfields = 4;
+metrics = {'events', 'delays', 'maxdescent'};
 for ii = 1:nF
 	res(ii).name = files(ii).name(strfind(files(ii).name, 'Seizure')+(7:8));
 	res(ii).data = load(fullfile(files(ii).folder, files(ii).name));
 	fields = fieldnames(res(ii).data);
-	res(ii).time = res(ii).data.(fields{whichfields(1)}).computeTimes / 1e3;
+	whichfields = find(sum(cell2mat(cellfun(@(f) strcmpi(f, fields), metrics, 'uni', 0)), 2));
+    whichfields = flip(whichfields);
+    res(ii).time = res(ii).data.(fields{whichfields(1)}).computeTimes / 1e3;
 	[res(ii).Z, res(ii).Vx, res(ii).Vy] = ...
 		deal(zeros(length(res(ii).data.(fields{whichfields(1)}).computeTimes), length(fields)));
 	for jj = 1:numel(fields)
@@ -34,7 +36,7 @@ for ii = 1:nF
 			end
 			res(ii).(f)(:, jj) = data;
             % Remove values where fit is not significant
-			res(ii).(f)(res(ii).data.(fields{jj}).p > .05, :) = nan;
+			res(ii).(f)(res(ii).data.(fields{jj}).p > sig, :) = nan;
             % Remove values where slope is zero in both directions
             res(ii).(f)(all(abs(res(ii).data.(fields{jj}).beta(1:2, :)) < eps), :) = nan;
 		end
@@ -42,15 +44,16 @@ for ii = 1:nF
 	ax(ii) = polaraxes();
 % 	ax(ii) = axes();
 	subplot(2, nF, ii, ax(ii));
-	tt = linspace(res(ii).time(1), res(ii).time(end), length(res(ii).time) * 10);
+	tt = (res(ii).time(1): 1e-3: res(ii).time(end));
 % 	data = interp1(res(ii).time, smoothdata(unwrap(res(ii).Z(:, whichvars)), 'movmean', 20), tt);
     Zu = unwrap(res(ii).Z(:, whichfields));
 %     valid = ~isnan(Zu);
 %     f = fit(res.time(valid)', Zu(valid), 'smoothingspline', 'smoothing', .05);
 	data = smoothdata(...
 		interp1(res(ii).time, Zu, tt), ...
-		'rlowess', 1/mean(diff(res(ii).time))^3, 'omitnan');
-	polarplot(ax(ii), data, tt, '-', 'linewidth', 2);
+		'movmean', 5 / (tt(2) - tt(1)), 'omitnan');
+	polarplot(ax(ii), data, tt - min(tt), '-', 'linewidth', 2);
+    ax(ii).ColorOrder = varycolor(5);
 % % 	plot3(ax(ii), cos(data), sin(data), tt); hold on
 % 	ax(ii).ColorOrderIndex = 1;
 % 	plot3(ax(ii), cos(res(ii).Z(:, whichfields)), ...
@@ -78,49 +81,71 @@ for kk = 1:numel(ax)
 end
 ttl = @(s) annotation('textbox', ...
     'string', s, ...
-    'Position', [.5 .98 0 0], ...
+    'Position', [.5 .05 0 0], ...
     'FontSize', 18, 'FontWeight', 'bold', 'HorizontalAlignment', 'center', ...
     'FitBoxToText', 'on', ...
     'LineStyle', 'none');
 ttl(pat)
-%%
-figure(2); clf; fullwidth(true);
-S = 1;
-metrics = [1 3:6];
-metrics = 1:4;
-pairs = nchoosek(metrics, 2);
+%% Comparison of metrics
+
+metrics = {'delays', 'events', 'maxdescent'};
+alltime = cell(numel(metrics), 1);
+% Make a matrix of all metrics from all seizures
+for seiz = 1:nF  % for each seizure
+    timetemp = res(seiz).time;
+    alltime{seiz} = (timetemp - min(timetemp)) / range(timetemp);
+    fields = fieldnames(res(seiz).data);
+    temp = res(seiz).Z(:, strcmpi(metrics{1}, fields));  % Get the first metric
+    for m = 2:numel(metrics)  % concatenate the remaining metrics as columns
+        temp = cat(2, temp, res(seiz).Z(:, strcmpi(metrics{m}, fields)));
+    end
+    if seiz == 1  % If this is the first seizure
+        alldata = temp;  % ... initialize alldata
+    else  % otherwise
+        alldata = cat(1, alldata, temp);  % ... concat data to rows
+    end
+end
+alltime = cat(1, alltime{:});  % make a long column of all time points 
+% ... in time relative to seizure progression
+
+
+pairs = nchoosek(1:numel(metrics), 2);  % Get each pair of metrics
 nP = size(pairs, 1);
-r = floor(sqrt(nP)); c = ceil(sqrt(nP));
-fields = fieldnames(res(S).data);
-for dd = 1:nP
-    ax = polaraxes();
-    subplot(r, c, dd, ax)
-    d1 = pairs(dd, 1); d2 = pairs(dd, 2);
-    data = res(S).Z(:, d1) - res(S).Z(:, d2);
-    edges = linspace(0, 2*pi, 41);
-    bc = histcounts(mod(data, 2*pi), edges);
-    bc = bc/max(bc) * res(S).time(end);
-    polarhistogram(ax, 'BinEdges', edges, 'BinCounts', bc); hold on;
-    polarplot(ax, data, res(S).time, '.'); hold off;
-    title(ax, sprintf('%s - %s', fields{d1}, fields{d2}))
+r = floor(sqrt(nP)); c = ceil(nP / r);  % Create an appropriate number of subplots
+figure(2); clf; fullwidth(r > 1);
+for dd = 1:nP  % For each pair
+    ax = polaraxes();  % Create a polar axis,
+    subplot(r, c, dd, ax)  % ... place it,
+    d1 = pairs(dd, 1); d2 = pairs(dd, 2);  % ... assign convenience vars,
+    data = alldata(:, d1) - alldata(:, d2);  % ... compute the difference between angles from each metric,
+    edges = linspace(0, 2*pi, 41);  % ... create 40 bins between 0 and 2pi,
+    bc = histcounts(mod(data, 2*pi), edges);  % ... get the counts per bin,
+    bc = bc/max(bc);  % ... scale to match time (relative), 
+    polarhistogram(ax, 'BinEdges', edges, 'BinCounts', bc); hold on;  % ... draw the histogram,
+    polarplot(ax, data, alltime, '.'); hold off;  % ... add the differences at each time
+    
+    % Prettify
+    title(ax, sprintf('%s - %s', metrics{d1}, metrics{d2}))
     axis tight
     ax.ThetaTickLabel = [];
 %     set(gca, 'thetaticklabels', [])
 end
-ttl(sprintf('%s Seizure%s', pat, strrep(res(S).name, '_', '')));
+ttl(sprintf('%s', pat));
 
 
 %% Blurred im
 
 dim = 100;
 figure(3); fullwidth()
-whichmetrics = 4;
+metrics = {'falling'};
 scale = @(x, M) round((x - min(x)) / range(x) * (M - 1) + 1);
-for field = 1:nF
-    ax = subplot(1, nF, field);
-    time = res(field).time(:); time = time - min(time);
-    xdata = time .* cos(res(field).Z(:, whichmetrics)); 
-    ydata = time .* sin(res(field).Z(:, whichmetrics)); 
+for file = 1:nF
+    fields = fieldnames(res(file).data);
+    whichfields = find(sum(cell2mat(cellfun(@(f) strcmpi(f, fields), metrics, 'uni', 0)), 2));
+    ax = subplot(1, nF, file);
+    time = res(file).time(:); time = time - min(time);
+    xdata = time .* cos(res(file).Z(:, whichfields)); 
+    ydata = time .* sin(res(file).Z(:, whichfields)); 
     valid = ~isnan(xdata);
     cdata = ones(size(ydata));
     inds = sub2ind([dim, dim], ...
