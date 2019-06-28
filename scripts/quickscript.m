7c92f9540e6d78c0f49bbd8b0ca19c2dadb27503
Emily <eds2@bu.edu>
2019-06-28 16:09:20 -0400

    Update quickscript.m

--- scripts/quickscript.m
+++ scripts/quickscript.m
@@ -3,7 +3,7 @@ compute_coherograms(pat, seizure, T);
 
 function compute_coherograms(pat, seizure, T)
 
-if isempty('T'); T = 10; end
+if isempty(T); T = 10; end
 datapath = genpath(['/projectnb/ecog/Data' filesep pat]);  % matlab doesn't follow symlinks so 
 addpath(datapath);  % ... add the original data path first
 patpath = genpath(pat);  % ... and then add the local patient path on top 
@@ -22,7 +22,7 @@ outfile = matfile(basename, 'writable', true);
 mea = exclude_channels(mea);
 
 skipfactor = floor(mea.SamplingRate / 1e3);
-data = double(downsample(mea.Data, skipfactor));
+data = single(downsample(mea.Data, skipfactor));
 Fs = mea.SamplingRate / skipfactor;
 
 % [~, mea] = filter_mea(mea, 'mua');
@@ -44,11 +44,18 @@ params.fpass = [0 500];  % lfp filtered range
 params.tapers = [W T 1]; 
 
 pairs = nchoosek(1:nCh, 2);
-data1 = data(:, pairs(1:500, 1));
-data2 = data(:, pairs(1:500, 2));
+numpairs = size(pairs, 1);
+padding = mea.Padding;
 
 outfile.data = data;
+outfile.position = mea.Position;
+outfile.badchannels = mea.BadChannels;
+outfile.pairs = pairs;
+outfile.params = params;
+outfile.movingwin = movingwin;
+
 clear data;
+clear mea;
 %% Initialize arrays
 % ii = length(pairs);
 % disp('Initializing arrays with last pair...')
@@ -66,36 +73,51 @@ clear data;
 % 	disp(['ii=' num2str(ii)]);
 % end
 
-for ii = 0:100:400
-    params.fpass = [ii ii+100];
+parfor ii = 1:(numpairs / 100)
     disp(ii)
+    pairs = nchoosek(nCh, 2);
+    i0 = (ii - 1) * 100 + 1;
+    iF = min(i0 + 99, numpairs);
+    pinds = i0:iF;
     tic
     [C, phi, ~, ~, ~, t, f, confC, ~] = ...
-        cohgramc(data1, data2, movingwin, params);
+        cohgramc(...
+            data(:, pairs(pinds, 1)), data(:, pairs(pinds, 2)), ...
+            movingwin, params);
     toc
-    outfile.(sprintf('C%03d', ii)) = C;
-    outfile.(sprintf('phi%03d', ii)) = phi;
-    outfile.(sprintf('f%03d', ii)) = f;
+    
+%     tic
+%     [C, phi, ~, ~, ~, t, f, confC, ~] = ...
+%         cohgramc(...
+%             data1, data2, ...
+%             movingwin, params);
+%     toc
+    
+    if ii == 1
+        Cfull = int16(C * 1e4);
+        phifull = int16(phi * 1e4);
+    else 
+        Cfull = cat(3, Cfull, int16(C * 1e4));  % scale C up and cast to int16
+        phifull = cat(3, phifull, int16(phi * 1e4));  % scale phi up and then cut it to int16
+    end
+%     outfile.(sprintf('C%03d', ii)) = int16(C * 1e4);
+%     outfile.(sprintf('phi%03d', ii)) = int16(phi / pi * 1e4);
+%     outfile.(sprintf('f%03d', ii)) = f;
     
     plotmean();  % nested plotting function
     
     clear C phi
 end
 disp('Saving result.')
-t = t - mea.Padding(1);  % correct for padding
+t = t - padding(1);  % correct for padding
 confC = confC(1);
 % Save results
-% outfile.C = C;
-% outfile.phi = phi;
+outfile.C = Cfull;
+outfile.phi = phifull;
 outfile.t = t;
 % outfile.f = f;
 outfile.confC = confC;
 % outfile.phistd = phistd;
-outfile.position = mea.Position;
-outfile.badchannels = mea.BadChannels;
-outfile.pairs = pairs;
-outfile.params = params;
-outfile.movingwin = movingwin;
 
 disp('Done.')
 
