6bcb0382ccd44bf2cc2723f9f11a53ee88c10a2a
Emily <eds2@bu.edu>
2019-07-07 11:44:37 -0400

    Create delays_by_localregression.m

---
+++ scripts/delays_by_localregression.m
@@ -0,0 +1,22 @@
+% delays_by_localregression
+
+dphi = permute(phi, [2 1 3]);  % permute array to f x t x pairs
+dphi = gradient(dphi, f(2) - f(1));  % d/df
+
+winsz = 3;  % Hz
+dim = 1; % frequency dimension
+nanflag = 'includenan';  % don't interpolate nans
+degree = 2;  % linear
+method = 'rlowess';
+
+delays = matlab.internal.math.localRegression(dphi, winsz, dim, ...
+            nanflag, degree, method, f);
+        
+%% Imagesc delays
+
+for ii = 1:10
+    temp = delays(:, :, ii);
+    imagesc(t, f, temp, quantile(temp(:), [.05, .95]));
+    axis xy; colorbar; ylim([0 100]); drawnow(); pause();
+end 
+
