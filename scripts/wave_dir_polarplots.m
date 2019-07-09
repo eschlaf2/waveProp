2f7c5eed0a0cf434a2b260d3267eb57d265faf20
Emily <eds2@bu.edu>
2019-07-08 20:39:22 -0400

    Update wave_dir_polarplots.m

--- scripts/wave_dir_polarplots.m
+++ scripts/wave_dir_polarplots.m
@@ -13,7 +13,7 @@ res(nF) = struct(...
 
 figure(1); clf; fullwidth(true);
 metrics = {'delays', 'events', 'delays_T02_fband25_50'};
-ax(nF * 2) = polaraxes();
+ax = gobjects(6);
 for ii = 1:nF
 	res(ii).name = strrep(files(ii).name(strfind(files(ii).name, 'Seizure')+(7:8)), '_', '');
 	res(ii).data = load(fullfile(files(ii).folder, files(ii).name));
