b97b4ce4a43a80c568692d895944d31e5a7fff54
Emily <eds2@bu.edu>
2019-07-16 16:16:46 -0400

    Update plot_wave_directions.m

--- plot_wave_directions.m
+++ plot_wave_directions.m
@@ -1,10 +1,9 @@
-function output = plot_wave_directions(mea, waveFit, sig)
+function output = plot_wave_directions(mea, waveFit, sig, ax)
 % Make a summary plot of wave directions 
 
 %% Parse input and set defaults
-if ~exist('sig', 'var')
-	sig = 0.05;
-end
+if ~exist('sig', 'var'), sig = 0.05; end
+if ~exist('ax', 'var'), ax = axes(); end
 
 plotTitle = strrep(waveFit.Name, '_', ' ');
 Z = waveFit.Z;  
@@ -18,22 +17,21 @@ computeTimes = waveFit.computeTimes / 1e3;
 
 %% Plot the firing rate and wave velocity at each discharge
     
-p = axes();
-title(p, plotTitle)
-xlabel(p, 'Time (s)')
+title(ax, plotTitle)
+xlabel(ax, 'Time (s)')
 
 % Left plot, right axis
 % Put wave direction labels on the right y-axis	
-yyaxis(p, 'right');  
-ylim(p, [-pi, pi]);  % set limits
-hold(p, 'on');
-yticks(p, [-pi, 0, pi]);  % Put ticks at pi rad
-ytks = p.YTick;  % store ticks (transform them to left axis)
+yyaxis(ax, 'right');  
+ylim(ax, [-pi, pi]);  % set limits
+hold(ax, 'on');
+yticks(ax, [-pi, 0, pi]);  % Put ticks at pi rad
+ytks = ax.YTick;  % store ticks (transform them to left axis)
 
 % Relabel the right axis using arrows
 % ylab = get(p, 'yticklabels');
 labs = {'\leftarrow'; '\rightarrow'; '\leftarrow'};
-p.YTickLabel = labs;
+ax.YTickLabel = labs;
 % ylabel(p, 'Wave direction (\pi rad)')
 
 % Transform Z and ytks so that you can use the left axis
@@ -44,40 +42,40 @@ b = x\y;
 
 % Left plot, left axis
 % Create grid lines corresponding to the right axis
-yyaxis(p, 'left')
+yyaxis(ax, 'left')
 i = 1;
 tempc = lines(2);  % grid line colors correspond to angle
 for y = ytks * pi
 	i = mod(i + 1, 2);
-	plot(p, [Time(1); Time(end)],  b' * [1; y] * [1; 1], ... 
+	plot(ax, [Time(1); Time(end)],  b' * [1; y] * [1; 1], ... 
 		':', 'color', tempc(1 + i, :))
 	hold on
 end
-xlim(p, [Time(1), Time(end)])
-ylim(p, x * b);
+xlim(ax, [Time(1), Time(end)])
+ylim(ax, x * b);
 
 % Plot the mean firing rate
-plot(p, Time, meanFr, 'color', .5*[1 1 1]); 
-ylabel(p, 'Mean firing rate (spikes/s)')
-p.XGrid = 'on';
-hold(p, 'on');
+plot(ax, Time, meanFr, 'color', .5*[1 1 1]); 
+ylabel(ax, 'Mean firing rate (spikes/s)')
+ax.XGrid = 'on';
+hold(ax, 'on');
 
 cmapDir = hsv;  % scatter color corresponds to direction
-colormap(p, cmapDir);
-p.CLim = [-pi pi];
+colormap(ax, cmapDir);
+ax.CLim = [-pi pi];
 	
 for i = 1:numWaves  % Overlay colored data points each discharge
 	if waveFit.p(i) >= sig
 		continue
 	end
-	yyaxis(p, 'left'); % right axis
-	scatter(p, computeTimes(i), b(1) + b(2) * Z(i), 40, waveFit.Z(i), 'filled');
+	yyaxis(ax, 'left'); % right axis
+	scatter(ax, computeTimes(i), b(1) + b(2) * Z(i), 40, waveFit.Z(i), 'filled');
 
 % 		drawnow()
 % 		pause(1e-2)
 end
-hold(p, 'off')
+hold(ax, 'off')
 
 
 %% return
-output = p;
+output = ax;
