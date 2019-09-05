32620e4660367d51c1931ff5192bce142096c843
Emily <eds2@bu.edu>
2019-09-05 12:12:21 -0400

    Update analyze_wave_directions.m

--- scripts/analyze_wave_directions.m
+++ scripts/analyze_wave_directions.m
@@ -1,4 +1,4 @@
-% pat = 'c7'; seizure = 1;
+% pat = 'SIM'; seizure = 9;
 datapath = genpath(['/projectnb/ecog/Data' filesep pat]);  % matlab doesn't follow symlinks so 
 addpath(datapath);  % ... add the original data path first
 patpath = genpath(pat);  % ... and then add the local patient path on top 
@@ -40,12 +40,12 @@ plot_wave_directions(mea, events);
 print(gcf, events.Name, '-dpng');
 outfile.events = events;
 
-% disp('Computing wave directions from maxdescent ...')
-% [maxdescent, mea] = wave_prop(mea, 'maxdescent', ...
-% 	'exclude', false, 'showplots', showplots);
-% plot_wave_directions(mea, maxdescent);
-% print(gcf, maxdescent.Name, '-dpng');
-% outfile.maxdescent = maxdescent;
+disp('Computing wave directions from maxdescent ...')
+[maxdescent, mea] = wave_prop(mea, 'maxdescent', ...
+	'exclude', false, 'showplots', showplots);
+plot_wave_directions(mea, maxdescent);
+print(gcf, maxdescent.Name, '-dpng');
+outfile.maxdescent = maxdescent;
 
 % disp('Computing wave directions from rising deviance ...')
 % [rising, mea] = wave_prop(mea, 'rising', 'exclude', false);
@@ -59,7 +59,7 @@ outfile.events = events;
 % print(gcf, falling.Name, '-dpng');
 % outfile.falling = falling;
 
-band = [1 50];
+band = [0 50];
 disp('Computing wave directions from delays ...')
 [delays, mea] = wave_prop(mea, 'delays', ...
 	'exclude', false, 'showplots', showplots, 'T', T, 'band', band);
@@ -68,21 +68,21 @@ print(gcf, delays.Name, '-dpng')
 outfile.(sprintf('delays_T%02d_fband%d_%d', T, band)) = delays;
 
 
-band = [25 50];
-disp('Computing wave directions from delays ...')
-[delays, mea] = wave_prop(mea, 'delays', ...
-	'exclude', false, 'showplots', showplots, 'T', T, 'band', band);
-plot_wave_directions(mea, delays);
-print(gcf, delays.Name, '-dpng')
-outfile.(sprintf('delays_T%02d_fband%d_%d', T, band)) = delays;
-
-band = [300 500];
-disp('Computing wave directions from delays ...')
-[delays, mea] = wave_prop(mea, 'delays', ...
-	'exclude', false, 'showplots', showplots, 'T', T, 'band', band);
-plot_wave_directions(mea, delays);
-print(gcf, delays.Name, '-dpng')
-outfile.(sprintf('delays_T%02d_fband%d_%d', T, band)) = delays;
+% band = [25 50];
+% disp('Computing wave directions from delays ...')
+% [delays, mea] = wave_prop(mea, 'delays', ...
+% 	'exclude', false, 'showplots', showplots, 'T', T, 'band', band);
+% plot_wave_directions(mea, delays);
+% print(gcf, delays.Name, '-dpng')
+% outfile.(sprintf('delays_T%02d_fband%d_%d', T, band)) = delays;
+% 
+% band = [300 500];
+% disp('Computing wave directions from delays ...')
+% [delays, mea] = wave_prop(mea, 'delays', ...
+% 	'exclude', false, 'showplots', showplots, 'T', T, 'band', band);
+% plot_wave_directions(mea, delays);
+% print(gcf, delays.Name, '-dpng')
+% outfile.(sprintf('delays_T%02d_fband%d_%d', T, band)) = delays;
 
 
 disp('Done.')
