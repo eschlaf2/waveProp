4780d3a752e4240c0e298b94c9ae3e3da4618584
Emily <eds2@bu.edu>
2019-09-09 14:46:15 -0400

    Update dblr.m

--- helpers/dblr.m
+++ helpers/dblr.m
@@ -4,11 +4,12 @@ dim = 1;  % frequency dimension
 nanflag = 'includenan';  % don't interpolate nans
 method = 'movmean';
 df = mean(diff(f));
+if ~exist('winsz', 'var') || isempty(winsz); winsz = 20; end  % winsz in samples to smooth over
 
 switch lower(delaytype)
 	case 'phase'
 		phif(Cf <= confC) = nan;  % set insignificant values to nan
-		delays = smoothdata(phif ./ f', dim, method, winsz / df, nanflag);
+		delays = smoothdata(phif ./ f', dim, method, winsz, nanflag);
 	case 'group'
 		[~, dphi] = gradient(unwrap(phif), df);  % Compute the gradient of phi wrt freq
 		dphi(Cf <= confC) = nan;  % set insignificant values to nan
