a7f90b337108d7fd1eb22e1624c25cf019b79e4d
Emily <eds2@bu.edu>
2019-09-19 15:59:38 -0400

    Update wave_dir_polarplots.m

--- scripts/wave_dir_polarplots.m
+++ scripts/wave_dir_polarplots.m
@@ -96,7 +96,7 @@ switch plotnum
                 R(idx) = abs(m1(idx));
 %                 R2e(idx) = N(idx) / (N(idx)-1) * (R2 - 1/N(idx));
                 K(idx) = R(idx) * (2 - R(idx)^2) / (1 - R(idx)^2);
-                d(idx) = 1 - mean(angle(dZ{idx})^2);
+                d(idx) = 1 - mean(angle(dZ{idx}).^2);
                 sigma(idx) = sqrt(d/N(idx)/R(idx)^2);
                 q(idx) = asin(sqrt(-log(sig))*sigma(idx));
 % 				R(idx) = abs(m1(idx));  % recall circular variance is 1 - R
