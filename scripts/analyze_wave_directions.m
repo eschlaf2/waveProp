b415a8b3038e392badfa7ec537b35440f2539843
Emily <eds2@bu.edu>
2019-09-25 17:10:15 -0400

    Update analyze_wave_directions.m

--- scripts/analyze_wave_directions.m
+++ scripts/analyze_wave_directions.m
@@ -32,53 +32,53 @@ outfile = matfile(outname, 'writable', true);
 
 %%
 
-disp('Computing wave directions from events ...')
-[events, mea] = wave_prop(mea, 'events', ...
-	'exclude', false, 'showplots', showplots);
-plot_wave_directions(mea, events);
-print(gcf, events.Name, '-dpng');
-outfile.events = events;
+% disp('Computing wave directions from events ...')
+% [events, mea] = wave_prop(mea, 'events', ...
+% 	'exclude', false, 'showplots', showplots);
+% plot_wave_directions(mea, events);
+% print(gcf, events.Name, '-dpng');
+% outfile.events = events;
+% 
+% disp('Computing wave directions from maxdescent ...')
+% [maxdescent, mea] = wave_prop(mea, 'maxdescent', ...
+% 	'exclude', false, 'showplots', showplots);
+% plot_wave_directions(mea, maxdescent);
+% print(gcf, maxdescent.Name, '-dpng');
+% outfile.maxdescent = maxdescent;
+% 
+% disp('Computing wave directions from rising deviance ...')
+% [rising, mea] = wave_prop(mea, 'rising', 'exclude', false);
+% plot_wave_directions(mea, rising);
+% print(gcf, rising.Name, '-dpng');
+% outfile.rising = rising;
+% 
+% disp('Computing wave directions from falling deviance ...')
+% [falling, mea] = wave_prop(mea, 'falling', 'thresh', -Inf, 'exclude', false);
+% plot_wave_directions(mea, falling);
+% print(gcf, falling.Name, '-dpng');
+% outfile.falling = falling;
+% 
+% disp('Computing wave directions from delays ...')
+% T = .2;
+% band = [0 50];
+% [delays, mea] = wave_prop(mea, 'delays', ...
+% 	'exclude', false, 'showplots', showplots, 'T', T, 'band', band);
+% plot_wave_directions(mea, delays);
+% print(gcf, delays.Name, '-dpng')
+% fieldname = checkname(sprintf('delays_T%02g_fband%d_%d', T, band));
+% outfile.(fieldname) = delays;
+% 
+% disp('Computing wave directions from delays (again) ...')
+% T = 1;
+% band = [1 50];
+% [delays, mea] = wave_prop(mea, 'delays', ...
+% 	'exclude', false, 'showplots', showplots, 'T', T, 'band', band);
+% plot_wave_directions(mea, delays);
+% print(gcf, delays.Name, '-dpng')
+% fieldname = checkname(sprintf('delays_T%02g_fband%d_%d', T, band));
+% outfile.(fieldname) = delays;
 
-disp('Computing wave directions from maxdescent ...')
-[maxdescent, mea] = wave_prop(mea, 'maxdescent', ...
-	'exclude', false, 'showplots', showplots);
-plot_wave_directions(mea, maxdescent);
-print(gcf, maxdescent.Name, '-dpng');
-outfile.maxdescent = maxdescent;
-
-disp('Computing wave directions from rising deviance ...')
-[rising, mea] = wave_prop(mea, 'rising', 'exclude', false);
-plot_wave_directions(mea, rising);
-print(gcf, rising.Name, '-dpng');
-outfile.rising = rising;
-
-disp('Computing wave directions from falling deviance ...')
-[falling, mea] = wave_prop(mea, 'falling', 'thresh', -Inf, 'exclude', false);
-plot_wave_directions(mea, falling);
-print(gcf, falling.Name, '-dpng');
-outfile.falling = falling;
-
-disp('Computing wave directions from delays ...')
-T = .2;
-band = [0 50];
-[delays, mea] = wave_prop(mea, 'delays', ...
-	'exclude', false, 'showplots', showplots, 'T', T, 'band', band);
-plot_wave_directions(mea, delays);
-print(gcf, delays.Name, '-dpng')
-fieldname = checkname(sprintf('delays_T%02g_fband%d_%d', T, band));
-outfile.(fieldname) = delays;
-
-disp('Computing wave directions from delays (again) ...')
-T = 1;
-band = [1 50];
-[delays, mea] = wave_prop(mea, 'delays', ...
-	'exclude', false, 'showplots', showplots, 'T', T, 'band', band);
-plot_wave_directions(mea, delays);
-print(gcf, delays.Name, '-dpng')
-fieldname = checkname(sprintf('delays_T%02g_fband%d_%d', T, band));
-outfile.(fieldname) = delays;
-
-disp('Computing wave directions from delays (and again) ...')
+disp('Computing wave directions from delays (T=10) ...')
 T = 10;
 band = [1 13];
 [delays, mea] = wave_prop(mea, 'delays', ...
