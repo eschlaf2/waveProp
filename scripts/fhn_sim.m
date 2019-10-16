1eff6cd41ffff687fb5ef01a74327b5deabdb6be
Emily <eds2@bu.edu>
2019-10-16 19:57:33 -0400

    Update fhn_sim.m

--- scripts/fhn_sim.m
+++ scripts/fhn_sim.m
@@ -1,25 +1,18 @@
 rng default
 defvar(who, 'SAVE', false);
 
-ncols=160;                               % Number of columns in domain
-nrows=160;                               % Number of rows in domain
-dt=.5;                                   % Time step (ms)
-dur=80e3/dt;                             % Number of time steps
-mindur=80e3/dt;
-Iex=.25;                                  % Amplitude of external current
-G = 1;                                  % Conductance
-a = 0.48; b = 0.8; phi = 0.17;  % FHN model parameters
-dvdt = @(v, r, I) v - v.^3/3 - r + I;  % Fast dynamics
-drdt = @(v, r) phi * (v + a - b * r);  % Slow dynamics;
+params = init_params();
+struct2var(params.model);
 
 v=zeros(nrows,ncols);                    % Initialize voltage array
 r=v;                                     % Initialize refractoriness array
-skipfactor=20/dt;                           % Save every nth frame
-chunk = 5 * 1e3/dt;  % Save results in chunks
 
-fname = strrep(sprintf('spiral_wave_%d', StimProtocol), '-', 'M');
-basename = sprintf('%s%s%s', fname, filesep, fname);
-
+fname = ['FHN' filesep 'FHN'];
+if ~exist('sim_num', 'var')
+	sim_num = numel(dir(sprintf('%s_Seizure*mat', fname)));
+end
+if ischar(sim_num), sim_num = str2double(sim_num); end
+basename = sprintf('%s%sFHN_%d', fname, filesep, sim_num);
 
 if SAVE
 	v_out=zeros(nrows,ncols, chunk, 'int16'); 
@@ -32,9 +25,9 @@ if ~exist(fname, 'dir'), mkdir(fname), end
 iex=zeros(nrows,ncols);
 
 % Define the seizure (stim location and duration)
-t = (dt / 1e3:dt / 1e3:80)';  % time (s)
-seizure = define_seizure(t);
-mea = floor([(nrows/2) (ncols/2)]);
+t = (dt/1e3 : dt/1e3 : 80)' - 10;  % time (s)
+seizure = define_seizure(t, params.seizure);
+mea_ctr = floor([(nrows/2) (ncols/2)]);
 noise =@() randn(nrows,ncols) * .01;
 
 % Setup image
@@ -43,7 +36,7 @@ colormap(hot); axis image off; th=title('');
 set(gcf,'position',[500 600 256 256],'color',[1 1 1],'menubar','none')
 hold on;
 patch('faces', [(1:4) 1], ...
-	'vertices', [mea - [5 5]; mea + [4 -5]; mea + [4 4]; mea + [-5 4]], ...
+	'vertices', [mea_ctr - [5 5]; mea_ctr + [4 -5]; mea_ctr + [4 4]; mea_ctr + [-5 4]], ...
 	'edgecolor', [1 0 0], ...
 	'facecolor', 'none', ...
 	'linewidth', 2);
@@ -57,7 +50,7 @@ done=0;              % Flag for while loop
 
 
 while ~done          % Time loop
-	inds = round(mea + seizure(n, [1 2]));
+	inds = round(mea_ctr + seizure(n, [1 2]));
 	iex(inds(1) + (-2:2), inds(2) + (-2:2)) = Iex * seizure(n, 3);
     
     % Create padded v matrix to incorporate Neumann boundary conditions
@@ -99,7 +92,7 @@ while ~done          % Time loop
 		m=1+round(63*v); m=max(m,1); m=min(m,64);
         set(ih,'cdata',m);
         set(th,'string', ...
-			sprintf('%0.1f  %0.2f   %0.2f',n*dt,v(mea(1),mea(2)),r(mea(1),mea(2))))
+			sprintf('%0.1f  %0.2f   %0.2f',n*dt,v(mea_ctr(1),mea_ctr(2)),r(mea_ctr(1),mea_ctr(2))))
         drawnow
    		k = k + 1;
     end
@@ -119,59 +112,140 @@ end
 
 close(gcf)
 
-function seizure = define_seizure(t)
+if SAVE
+	mea = convert_to_mea_data(basename);
+	mea.seizure = seizure;
+	mea.fhn_params = params;
+	save(mea.Path, '-struct', 'mea');
+	convert_to_wavefit_data(mea);
+end
+
+function [res, p] = init_params(varargin)
+p = inputParser;
+add =@(varargin) addParameter(p, varargin{:});
+p.KeepUnmatched = true;
+
+% Integration parameters
+add('ncols', 160);                               % Number of columns in domain
+add('nrows', 160);                               % Number of rows in domain
+add('dt', .5);                                   % Time step (ms)
+add('dur', 160e3);                             % Number of time steps
+add('mindur', 160e3);
+
+% Coupling parameters
+add('Iex', .25);                                  % Amplitude of external current
+add('G', 1);                                  % Conductance
+
+% FHN parameters
+add('a', 0.48); 
+add('b', 0.8); 
+add('phi', 0.17);
+
+% FHN dynamics
+add('dvdt', @(v, r, I) v - v.^3/3 - r + I);  % Fast dynamics
+add('drdt', '');
+
+% Video and output params
+add('skipfactor', 40);    % Save every nth frame
+add('chunk', 1e4);  % Save results in chunks
+
+parse(p, varargin{:});
+res.model = p.Results;
+
+if isempty(res.drdt)
+	res.model.drdt = @(v, r) res.phi * (v + res.a - res.b * r);
+end
+
+
+%% Seizure params
+p = inputParser;
+add =@(varargin) addParameter(p, varargin{:});
+p.KeepUnmatched = true;
+
+% Length of each stage (in seconds)
+validate_tau = @(x) numel(x) == 5 || numel(x) == 4;
+add('tau', [0 20 20 10 10]', validate_tau);  % Seizure stages
+
+% Stim duration params
+add('dss', .005);  % Single spike
+add('d0', .07);  % Short burst
+add('df', .2);  % Long burst
+
+% Radius function
+add('r0', 10);
+add('rf', 70);
+
+% Angle function
+add('theta0', -pi/2);
+add('thetaf', 0);
+
+% Stim times
+add('N', 100);  % Number of discharges
+add('noise', .01);  % sd (in ms)
+
+parse(p, varargin{:});
+res.seizure = p.Results;
+
+end
+
+function [seizure, R, theta] = define_seizure(t, params)
 
 stages = {'pre', 'moving away', 'moving around', 'constant burst', 'increasing burst'};
-tau = [10 20 20 10 10]';  % Seizure stages
-tau = cumsum(tau);
+
+struct2var(params);
+
+tau = cumsum(tau(:));
 
 % Linear mapping function [t0, tf] -> [0, 1]
 T =@(t0, tf, t) (t - t0) ./ (tf - t0);  
 
 % Stim duration function
-dss = .005;  % Single spike
-d0 = .07;  % Short burst
-df = .2;  % Long burst
 D =@(t) ...  
 	dss * (t > tau(1) & t <= tau(3)) + ...
 	d0 * (t > tau(3) & t <= tau(4)) + ...
 	(d0 + (df - d0) * T(tau(4), tau(5), t)) .* (t > tau(4) & t <= tau(5));
 
 % Radius function
-r0 = 10;
-rf = 70;
 R =@(t) ...
 	(r0 + (rf - r0) * T(tau(1), tau(2), t)) .* (t > tau(1) & t <= tau(2)) + ...
 	(rf) * (t > tau(2));
 
 % Angle function
-theta0 = -pi/2;
-thetaf = 0;
 theta =@(t) ...
 	theta0 * (t <= tau(2)) + ...
 	(theta0 + (thetaf - theta0) .* T(tau(2), tau(3), t)) .* (t > tau(2) & t <= tau(3)) + ...
 	thetaf * (t > tau(3));
 
 % Stim times
-N = 100;  % Number of discharges
-noise = .01;  % sd (in ms)
 Ss = linspace(tau(1), tau(5), N)' + noise*randn(N, 1);  % starts
 Sf = Ss + D(Ss);  % ... and ends
 
 % Prep output
 posx = R(t) .* cos(theta(t));  % Stim position (relative to mea; pos in units of electrodes)
 posy = R(t) .* sin(theta(t));
-I = mod(sum((t >= Ss') + (t <= Sf'), 2), 2);  % Stim indicator
+Istim = mod(sum((t >= Ss') + (t <= Sf'), 2), 2);  % Stim indicator
 
 % Stimulus map
-seizure = [posx posy I];
+seizure = [posx posy Istim];
+
 end
 
-function convert_to_mea_data
+function mea = convert_to_mea_data(fname, sim_num, t)
+mea.BadChannels = [];
+mea.Time = t;
+mea.Name = ['FHN ' num2str(sim_num)];
+mea.Padding = [10 10];
+mea.Duration = t(end) - mea.Padding(2);
+mea.SamplingRate = 2e3;
+[xx, yy] = meshgrid(1:10, 1:10);
+mea.Position = [xx(:) yy(:)];
+mea.Path = sprintf(...
+	'%s%s%s_Seizure%02d_Neuroport_10_10.mat', ...
+	pwd, filesep, fname, sim_num);
 
-files = dir('spiral_wave_4/*mat');
-addpath('spiral_wave_4/');
-chunksize = 1e4;
+files = dir([fname '*mat']);
+% addpath('spiral_wave_4/');
+% chunksize = 1e4;
 nf = length(files);
 
 mea_addy = [80, 80];
@@ -191,5 +265,36 @@ for ii = 0:nf - 1
 		mea.Data(:, :, (ii*chunksize + 1):((ii+1)*chunksize)) = -temp;
 end
 mea.Data = reshape(mea.Data, 100, [])';
+
+end
+
+function convert_to_wavefit_data(mea)
+
+[~, fname, ~] = fileparts(mea.Path);
+finfo = strsplit(fname, '_');
+pat = finfo{1}; seizure = finfo{2}(8:end);
+fits = matfile([fname '_wave_prop_1.mat'], 'writable', true);
+plotnum = 0; wave_dir_polarplots;  % this should produce res
+
+time = mea.Time();
+S = mea.seizure;
+onset_inds = find(diff(S(:, 3)) > 0) + 1;
+onset_times = time(onset_inds);
+nearinds = interp1(onset_times, 1:length(onset_times), res.time, ...
+	'nearest', 'extrap');
+
+onset_times = onset_times(nearinds);
+onset_inds = onset_inds(nearinds);
+
+gt.computeTimes = res.time * 1e3;
+gt.Z = angle(-S(onset_inds, 1:2) * [1; 1j]);
+gt.speed = abs(S(onset_inds, 1:2) * [1; 1j]) ./ (res.time - onset_times');
+gt.V = [gt.speed .* cos(gt.Z), gt.speed .* sin(gt.Z)]';
+gt.p = 0 * nearinds;
+gt.beta = pinv(gt.V');
+
+fits.groundtruth = gt;
+
+
 
 end
