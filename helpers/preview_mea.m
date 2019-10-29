fc7ad382254f7e644ae85b525fcb05cef8d7a87e
Emily <eds2@bu.edu>
2019-10-29 13:09:11 -0400

    Add explanation

--- helpers/preview_mea.m
+++ helpers/preview_mea.m
@@ -1,4 +1,9 @@
 function preview_mea(mea, t_start, skipfactor, pause_length)
+% Inputs:
+%    mea (required)
+%    t_start (default: 0): time (s) to start preview
+%    skipfactor (default: 100): how to skip through frames
+%    pause_length (default: 0): pause time (s) between rendering frames
 
 defvar('skipfactor', 100);
 defvar('pause_length', 0);
