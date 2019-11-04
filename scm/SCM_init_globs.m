fc6c5f28c68a96b3355e54df38229d3afbe2b631
Emily <eds2@bu.edu>
2019-11-03 21:54:17 -0500

    minor

--- scm/SCM_init_globs.m
+++ scm/SCM_init_globs.m
@@ -15,20 +15,28 @@ function H = SCM_init_globs
 % Modified by M. A. Kramer for the Seizing Cortical Model (SCM)
 % May 2016.
 
-H.gamma_e = 170;                                % EPSP decay rate (/s)
-H.gamma_i = 50;                                 % IPSP decay rate (/s)
+H.tau_e = 0.02;			% excit neuron time-constant (/s) [original = 0.04 s]
+H.tau_i = 0.02;			% inhib neuron time-constant (/s) [original = 0.04 s]
+
+% voltage limits
+[H.Ve_rev,  H.Vi_rev]  = deal(0, -70);          % reversal potential (mV)
+[H.Ve_rest, H.Vi_rest] = deal(-64, -64);        % resting potential (mV)
+
+% gain per synapse at resting voltage (millivolt.sec)
+[H.rho_e, H.rho_i] = deal(1.00e-3, -1.05e-3);
+
+% E/IPSP rate constants
+H.gamma_e = 170;  % EPSP decay rate (/s)
+H.gamma_i = 50;  % IPSP decay rate (/s)
+
+% gap-junction diffusive-coupling strength
+H.D2 = .8;          % i <--> i initial in all space (cm^2) [0, 1]
+H.D1 = H.D2 / 100;  % e <--> e initial in all space (cm^2)
 
 % sigmoid characteristics
 [H.Qe_max,  H.Qi_max]  = deal(30,    60);       % sigmoid maximum (s^-1)
 [H.theta_e, H.theta_i] = deal(-58.5, -58.5);	% sigmoid threshold (mV)
 [H.sigma_e, H.sigma_i] = deal(3.0,   5.0);		% sigmoid 'width' (mV)
-
-% gain per synapse at resting voltage (millivolt.sec)
-[H.rho_e, H.rho_i] = deal(1.00e-3, -1.05e-3);
-
-% voltage limits
-[H.Ve_rev,  H.Vi_rev]  = deal(0, -70);          % reversal potential (mV)
-[H.Ve_rest, H.Vi_rest] = deal(-64, -64);        % resting potential (mV)
 
 % connectivities: j-->k convention (dimensionless)
 [H.Nee_a, H.Nei_a] = deal(2000, 2000);			% cortico-cortical
@@ -41,8 +49,14 @@ H.Nee_ab = H.Nee_a + H.Nee_b;
 H.Nei_ab = H.Nei_a + H.Nei_b;
 
 % default subcortical fluxes
-H.phi_ee_sc = H.Nee_sc * H.Qe_max;
-H.phi_ei_sc = H.Nei_sc * H.Qe_max;
+H.phi_ee_sc = H.Nee_sc * H.Qe_max;  % original [300]
+H.phi_ei_sc = H.Nei_sc * H.Qe_max;  % original [300]
+
+% axonal conduction velocity (cm/s), 
+H.v = 280;  % [original = 140 cm/s]
+
+% inverse-length scale for connectivity (/cm)
+H.Lambda = 4.0;			
 
 % d/dV derivatives of psi_ij weighting functions
 H.d_psi_ee = -1/(H.Ve_rev - H.Ve_rest);
@@ -50,10 +64,6 @@ H.d_psi_ei = -1/(H.Ve_rev - H.Vi_rest);
 H.d_psi_ie = -1/(H.Vi_rev - H.Ve_rest);
 H.d_psi_ii = -1/(H.Vi_rev - H.Vi_rest);
 
-H.v = 280;                 % axonal conduction velocity (cm/s), [original = 140 cm/s]
-H.Lambda = 4.0;			% inverse-length scale for connectivity (/cm)
-H.tau_e = 0.02;			% excit neuron time-constant (/s) [original = 0.04 s]
-H.tau_i = 0.02;			% inhib neuron time-constant (/s) [original = 0.04 s]
 
 return
 
