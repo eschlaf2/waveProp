9313744e4818d044e36155f312d239158c83b3c3
Emily <eds2@bu.edu>
2019-11-24 20:48:53 -0500

    Update scm_sim.m

--- scm/scm_sim.m
+++ scm/scm_sim.m
@@ -156,6 +156,7 @@ function convert_to_mea(PM)
 	mea.firingRate = reshape(qe_mat, size(mea.Data));
 	mea.event_inds = rate2events(mea);
 	mea.event_mat_size = size(mea.Data);
+	mea.firingRate = mua_firing_rate(mea);
 	fprintf('Saving %s ... ', mea.Path);
 	save(mea.Path, '-struct', 'mea');
 	m = matfile(sprintf('%s_%d_info', PM.basename, PM.sim_num), 'Writable', true);
