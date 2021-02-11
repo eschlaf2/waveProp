4596d23991487f49df4f854f20ae87e6478f271a
Emily <eds2@bu.edu>
2021-02-11 12:56:03 -0500

    Create update_map_smooth.m

---
+++ scm/update_map_smooth.m
@@ -0,0 +1,42 @@
+function [map,state] = update_map_smooth(state, expansion_rate, excitability_map, dt)
+
+%% Initialization
+if isstruct(state) 
+	Nx = state.grid_size(1); Ny = state.grid_size(2);
+
+	%center of initial source map.
+	xCenter = state.stim_center(1); 
+	yCenter = state.stim_center(2); 
+
+	map = false(Nx,Ny);
+
+	%set the initial map.
+% 	map(xCenter + (-1:1), yCenter + (-1:1)) = 1;
+	map(xCenter, yCenter) = 1;
+	state = zeros([Nx, Ny]);
+	state(map) = 1;
+	return
+end
+
+%% Update
+
+if expansion_rate <= 0, map = logical(state); return; end
+
+% boundary = zeros(size(state));
+boundary = conv2(state > 0, [0 1 0; 1 -4 1; 0 1 0], 'same') > 0;
+if ~exist('excitability_map', 'var'); excitability_map = ones(size(state)); end
+
+p_wavefront = 2^(0.5 * expansion_rate) - 1;  % 50% of area is recruited at this rate
+boundary = boundary .* excitability_map;
+
+dice = rand(size(state));
+wavefront = (dice < p_wavefront) & boundary & (state == 0);
+
+recruited = state >= 1;
+state(recruited) = state(recruited) + dt;
+
+state(wavefront) = 1;
+map = state >= 1 & state <= 1 + excitability_map;
+
+
+end
